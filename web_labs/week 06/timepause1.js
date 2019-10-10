c7f8ddeea5caccdbcafda49f77bb1fce50eb6b58
grishutenko <36900540+grishutenko@users.noreply.github.com>
2019-10-10 12:52:56 +0300

    задания 6 недели

---
+++ web_labs/week 06/timepause1.js
@@ -0,0 +1,3 @@
+let pause = require('goss_pause');
+console.time('g');
+pause(5000) || console.timeEnd('g');
---
+++ web_labs/week 06/timepause2.js
@@ -0,0 +1,5 @@
+console.time('g');
+function time(){
+console.timeEnd('g');
+}
+setTimeout(time, 5000);
